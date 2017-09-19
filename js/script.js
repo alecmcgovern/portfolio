a5b66c8c3189ba2bd88d79a9a52ceed4e85a971a
Alec M <alecmcgovern444@gmail.com>
2017-09-19 15:33:41 -0700

    added array of images for visual section

--- js/script.js
+++ js/script.js
@@ -2,5 +2,32 @@ $('document').ready(function(){
 
 	$('#navbar').localScroll({duration:800});
 
-	
+	const arrayLength = 8;
+
+	var picture = document.getElementById("picture-focused");
+
+	var swipeLeft = function() {
+		var picNumber = parseInt(picture.src.slice(-5).charAt(0));
+		var newPicNumber = (picNumber - 1) > -1 ? picNumber -1 : arrayLength - 1;
+
+
+		var newImageSrc = "images/array/" + newPicNumber + ".png";
+		picture.src = newImageSrc;
+	}
+
+	var swipeRight = function() {
+		var picNumber = parseInt(picture.src.slice(-5).charAt(0));
+		var newPicNumber = (picNumber + 1) >= arrayLength ? 0 : picNumber + 1;
+
+		console.log(newPicNumber);
+
+
+		var newImageSrc = "images/array/" + newPicNumber + ".png";
+		picture.src = newImageSrc;
+	}
+
+
+	document.getElementById("left-swipe").addEventListener("click", swipeLeft);
+
+	document.getElementById("right-swipe").addEventListener("click", swipeRight);
 });
